a3a5b37687da76a356b123338e857664189c38c0
shivam-chaurasiya <sschaurasiya5783@gmail.com>
2023-01-30 17:53:28 +0530

    node day 4

---
+++ main.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json({ extended: false }));
+app.get("/", (req, res) => res.send("Server up and running"));
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+});
